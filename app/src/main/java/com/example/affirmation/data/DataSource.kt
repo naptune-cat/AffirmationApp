fb4ad7ccb687e72b27644b06e3d40585be8f98d8
zoya <mkhanzoya8866@gmail.com>
2023-11-04 21:51:04 +0530

    h

--- app/src/main/java/com/example/affirmation/data/DataSource.kt
+++ app/src/main/java/com/example/affirmation/data/DataSource.kt
@@ -8,16 +8,16 @@ class Datasource {
 
     fun loadAffirmations(): List<Affirmation> {
         return listOf<Affirmation>(
-            Affirmation(R.string.affirmation1),
-            Affirmation(R.string.affirmation2),
-            Affirmation(R.string.affirmation3),
-            Affirmation(R.string.affirmation4),
-            Affirmation(R.string.affirmation5),
-            Affirmation(R.string.affirmation6),
-            Affirmation(R.string.affirmation7),
-            Affirmation(R.string.affirmation8),
-            Affirmation(R.string.affirmation9),
-            Affirmation(R.string.affirmation10)
+            Affirmation(R.string.affirmation1 , R.drawable.img1),
+            Affirmation(R.string.affirmation2 , R.drawable.img2),
+            Affirmation(R.string.affirmation3, R.drawable.img3),
+            Affirmation(R.string.affirmation4 , R.drawable.img4),
+            Affirmation(R.string.affirmation5 , R.drawable.img5),
+            Affirmation(R.string.affirmation6 , R.drawable.img6),
+            Affirmation(R.string.affirmation7 , R.drawable.img7),
+            Affirmation(R.string.affirmation8 , R.drawable.img8),
+            Affirmation(R.string.affirmation9 , R.drawable.img9),
+            Affirmation(R.string.affirmation10 , R.drawable.img10)
         )
     }
 }
